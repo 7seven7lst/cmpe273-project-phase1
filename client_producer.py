fc2725c4d8e129d2eb431bce300fbc42d62122f6
S Li <sevenlist0110@gmail.com>
2020-11-19 20:28:32 -0800

    phase1

---
+++ client_producer.py
@@ -0,0 +1,82 @@
+import zmq
+import time
+import sys
+from itertools import cycle
+
+import hrw
+import consistent_hashing
+
+def create_clients(servers):
+    producers = {}
+    context = zmq.Context()
+    for server in servers:
+        print(f"Creating a server connection to {server}...")
+        producer_conn = context.socket(zmq.PUSH)
+        producer_conn.bind(server)
+        producers[server] = producer_conn
+    return producers
+
+
+def generate_data_round_robin(servers):
+    print("Starting...")
+    producers = create_clients(servers)
+    pool = cycle(producers.values())
+    for num in range(10):
+        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        next(pool).send_json(data)
+        time.sleep(1)
+    print("Done")
+
+
+def generate_data_consistent_hashing(servers):
+    print("Starting...")
+    ## TODO
+    producers = create_clients(servers)
+    nodes = []
+    for key, value in producers.items():
+        nodes.append(consistent_hashing.Node(value, key))
+    client_ring = consistent_hashing.ConsistentHashRing(nodes, 3)
+    client_ring.setKeys()
+
+    for num in range(10):
+        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        node = client_ring.get_node(data['key'])
+        node.node.send_json(data)
+        time.sleep(1)
+    print("Done")
+
+def generate_data_hrw_hashing(servers):
+    print("Starting...")
+    ## TODO
+    producers = create_clients(servers)
+    counter = 1
+    nodes = []
+    for key, value in producers.items():
+        nodes.append(hrw.Node(value, counter))
+        counter +=1
+    for num in range(10):
+        data = { 'key': f'key-{num}', 'value': f'value-{num}' }
+        print(f"Sending data:{data}")
+        node = hrw.determine_responsible_node(nodes, data['key'])
+        node.node.send_json(data)
+        time.sleep(1)
+    print("Done")
+
+
+if __name__ == "__main__":
+    servers = []
+    num_server = 1
+    if len(sys.argv) > 1:
+        num_server = int(sys.argv[1])
+        print(f"num_server={num_server}")
+
+    for each_server in range(num_server):
+        server_port = "200{}".format(each_server)
+        servers.append(f'tcp://127.0.0.1:{server_port}')
+
+    print("Servers:", servers)
+    generate_data_round_robin(servers)
+    generate_data_consistent_hashing(servers)
+    generate_data_hrw_hashing(servers)
